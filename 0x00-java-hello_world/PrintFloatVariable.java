176b43e74f0bf334dfad603429b1404e7dce990b
isguliyev <guliyev-islam@proton.me>
2025-09-29 18:25:08 +0400

    Java – Creating Your First Program

---
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -0,0 +1,6 @@
+public class PrintFloatingVaiable {
+    public static void main() {
+        float pi = 3.14159f;
+        System.out.printf("Pi Value: %.3f\n", pi);
+    }
+}
---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+    public static void main() {
+        String text = "Hello, World!";
+        System.out.printf("%1$s\n%1$s\n%2$s\n", text, text.substring(0, 9));
+    }
+}
