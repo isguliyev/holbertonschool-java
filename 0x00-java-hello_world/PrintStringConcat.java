0319ebf5e8b2d9b4a3165db5fa5af0ff3b511eb1
isguliyev <guliyev-islam@proton.me>
2025-10-07 20:32:44 +0400

    Java – Creating Your First Program

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main() {
+        String text1 = "Hello, ";
+        String text2 = "World!";
+        String completeText = text1 + text2;
+        System.out.printf("Welcome, %s\n", completeText);
+    }
+}
